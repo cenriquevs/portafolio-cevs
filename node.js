1d0e7969163acee487b48c514d17dfaf90a54afc
cenriquevs <91174500+cenriquevs@users.noreply.github.com>
2022-09-07 19:05:59 -0600

    Update node.js

--- node.js
+++ node.js
@@ -3,7 +3,7 @@ window.addEventListener("scroll", function(){
     hrf.classList.toggle("active" , window.scrollY >=450)
 })
 
-var imagenes =['img/D6.jpg','img/D4.jpg','img/D5.jpg','img/D3.jpg','img/d2.jpg','img/cloud.jpg','img/D1.jpg']
+var imagenes =['img/D1.jpg','img/D2.jpg','img/D3.jpg','img/D4.jpg','img/D5.jpg','img/D6.jpg','img/D7.jpg','img/D8.jpg','img/D9.jpg','img/D10.jpg','img/D11.jpg','img/D12.jpg','img/D13.jpg','img/D14.jpg','img/D15.jpg','img/D16.jpg','img/D17.jpg','img/D18.jpg','img/D19.jpg','img/D20.jpg','img/D21.jpg','img/D22.jpg','img/D23.jpg','img/D24.jpg','img/D25.jpg','img/D26.jpg','img/D27.jpg','img/D28.jpg','img/D29.jpg']
 var cont = 0;
 
 function carruosel(contenedor){
